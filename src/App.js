631e834101b756beb0fe40d8bb77bf4536c2726e
msubin <joy120594@gmail.com>
2022-10-01 16:22:23 -0700

    Complete landing - sign in page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Component } from 'react';
 import './App.css';
 import LandingPageComponent from './components/LandingPageComponent';
+import SignIn from './components/LoginComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <LandingPageComponent />
-    </div>
-  );
+class App extends Component {
+  state = {
+    redirect: false
+  }
+
+  componentDidMount() {
+    this.id = setTimeout(() => this.setState({ redirect: true }), 3000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.id);
+  }
+
+  render() {
+    return this.state.redirect
+      ? <SignIn /> : <LandingPageComponent />;
+  }
 }
 
 export default App;
