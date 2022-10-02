b9021c4d42fde0305e76a71e5119dd79cf7686ff
Heather Lee <leeh1102@gmail.com>
2022-10-02 02:24:45 -0700

    update schema and db

--- server.js
+++ server.js
@@ -12,81 +12,39 @@ async function main() {
 }
 
 const path = require("path");
+const { StringDecoder } = require("string_decoder");
 
 const port = process.env.PORT || 8000;
 
 // schema per data type
-
 // User
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    userId: String,
-    postIds: [String]
+    userName: String,
+    password: String
 });
 const Users = mongoose.model('users', userSchema)
 
-// deliveryType - Vertual or in-person
-const deliveryTypeSchema = new mongoose.Schema({
-    deliveryType: String,
-})
-const deliveryTypes = mongoose.model('delivery_types', deliveryTypeSchema);
-
-// typeOfEvent
-const eventTypeSchema = new mongoose.Schema({
-    eventType: String,
-})
-const eventTypes = mongoose.model('event_types', eventTypeSchema);
-
-// Locations
-const sapLocationSchema = new mongoose.Schema({
-    sapLocation: String,
-})
-const sapLocations = mongoose.model('sap_locations', sapLocationSchema);
-
-
 // Posts
-// const postSchema = new mongoose.Schema({
-//     postId: String,
-//     userId: [Users.userId],
-//     title: String,
-//     location: { String },
-//     description: String,
-//     comment: {
-//         "postId": String,
-//         "content": String,
-//         "userId": [Users.userId],
-//         "Date": Date.now(),
-//     },
-//     likes: Boolean,
-//     eventTime: String,
-//     deliveryType: [deliveryTypes.deliveryType],
-//     sapLocation: [sapLocations.sapLocation],
-// })
-// const Posts = mongoose.model('posts', postSchema);
-
-// Comments
-// const commentSchema = new mongoose.Schema({
-//     comment: String,
-//     createdTime: Date.now(),
-//     userId: [Users.userId],
-//     postId: [Posts.postId],
-
-// })
-// const comments = mongoose.model('comments', commentSchema);
-
-// // title of events
-// const eventTitleSchema = new mongoose.Schema({
-//     evenTitle: String
-// })
-// const evenTitles = mongoose.model('evenTitles', eventTitleSchema);
-
-// EventImages
-const eventImageSchema = new mongoose.Schema({
-    image: String,
-    name: String
+const postSchema = new mongoose.Schema({
+    userId: String,
+    title: String,
+    location: String,
+    description: String,
+    comment: [{
+        postId: String,
+        content: String,
+        userId: String,
+        createdAt: Date,
+    }],
+    likes: Number,
+    startTime: String,
+    endTime: String,
+    deliveryType: String,
+    sapLocation: String,
+    postCreatedAt: String,
+    isAmenity: Boolean,
 })
-const eventImages = mongoose.model('event_images', eventImageSchema);
+const Posts = mongoose.model('posts', postSchema);
 
 // User CRED
 app.get('/api/users', (req, res) => {
@@ -98,7 +56,7 @@ app.get('/api/users', (req, res) => {
         }).catch(err => {
             res.status(500);
             res.json({ "status": "error", "msg": err.message });
-        });;
+        });
     } else {
         Users.find().then(results => {
             res.status(200);
@@ -106,15 +64,269 @@ app.get('/api/users', (req, res) => {
         });
     }
 })
-
+app.post('/api/users', (req, res) => {
+    const newUser = Users();
+    newUser.userName = req.body.userName;
+    newUser.password = req.body.password;
+    newUser.save().then(() => {
+        res.status(200);
+        res.json({ "status": "ok" });
+    }).catch(err => {
+        res.status(500);
+        res.json({ "status": "error", "msg": err.message });
+    });
+})
 // Posts CRED
-// Deliver_types CRED
-// Event_images CRED
-// Event_types CRED
-// Locations CRED
-// Events CRED
-// EventImages CRED
+app.get('/api/posts', (req, res) => {
+    if (req.query.postId && req.query.postId != "") {
+        Posts.find({ postId: req.query.postId }).then(results => {
+            res.status(200);
+            res.json(results);
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });
+    } else if (req.query.userId && req.query.userId != "") {
+        // /api/posts?userId=123123dkfjei
+        Posts.find({ userId: req.query.userId }).then(results => {
+            res.status(200);
+            res.json(results);
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });
+    }
+    else {
+        Posts.find().then(results => {
+            res.status(200);
+            res.json(results);
+        });
+    }
+})
 
+
+app.post('/api/posts', (req, res) => {
+    const newPost = Posts();
+    newPost.userId = req.body.userId;
+    newPost.title = req.body.title;
+    newPost.location = req.body.location;
+    newPost.description = req.body.description;
+    newPost.comment = [];
+    newPost.likes = 0;
+    newPost.deliveryType = req.body.deliveryType;
+    newPost.sapLocation = req.body.sapLocation;
+    newPost.startTime = req.body.startTime;
+    newPost.endTime = req.body.endTime;
+    newPost.isAmenity = req.body.isAmenity;
+    newPost.postCreatedAt = new Date();
+    newPost.save().then(() => {
+        res.status(200);
+        res.json({ "status": "ok" });
+    }).catch(err => {
+        res.status(500);
+        res.json({ "status": "error", "msg": err.message });
+    });;
+})
+
+
+// creating likes increments
+app.post('/api/likes', (req, res) => {
+    if (req.query.postId && req.query.postId != "") {
+        Posts.findOne({ postId: req.query.postId }).then(result => {
+            result.likes += 1;
+            Posts.findOneAndUpdate({ _id: req.query.postId }, result).then(() => {
+                res.status(200);
+                res.json({ "status": "ok" });
+            }).catch(err => {
+                res.status(500);
+                res.json({ "status": "error", "msg": err.message });
+            });
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });
+    } else {
+        Posts.find().then(results => {
+            res.status(200);
+            res.json(results);
+        });
+    }
+})
+
+// Comments CRED
+app.post('/api/comments', (req, res) => {
+    Posts.findOne({ _id: req.body.postId }).then(result => {
+        const newComment = {};
+        newComment.content = req.body.content;
+        newComment.createdAt = new Date();
+        newComment.userId = req.body.userId;
+        result.comment.push(newComment);
+        result.save().then(() => {
+            res.status(200);
+            res.json({ "status": "ok" });
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });
+    }).catch(err => {
+        res.status(500);
+        res.json({ "status": "error", "msg": err.message });
+    });
+})
+
+app.put('/api/comments', (req, res) => {
+    Posts.findOne({ _id: req.body.postId }).then(result => {
+        if (result != null) {
+            const commentIndex = result.comment.findIndex(element => element._id == req.body._id);
+            if (commentIndex >= 0) {
+                result.comment[commentIndex].content = req.body.content;
+                result.save().then(() => {
+                    res.status(200);
+                    res.json({ "status": "ok" });
+                }).catch(err => {
+                    res.status(500);
+                    res.json({ "status": "error", "msg": err.message });
+                });
+            } else {
+                res.status(500);
+                res.json({ "status": "error", "msg": "Post cannot be found" });
+            }
+        } else {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        }
+    }).catch(err => {
+        res.status(500);
+        res.json({ "status": "error", "msg": err.message });
+    });
+})
+
+app.delete('/api/comments', (req, res) => {
+    const targetPostId = req.query.postId;
+    const targetCommentId = req.query.commentId;
+    if (targetPostId && targetPostId != "" && targetCommentId && targetCommentId != "") {
+        Posts.findOne({ _id: targetPostId }).then(result => {
+            if (result != null) {
+                const commentIndex = result.comment.findIndex(element => element._id == targetCommentId);
+                result.comment.splice(commentIndex, 1)
+                if (commentIndex >= 0) {
+                    result.save().then(() => {
+                        res.status(200);
+                        res.json({ "status": "ok" });
+                    }).catch(err => {
+                        res.status(500);
+                        res.json({ "status": "error", "msg": err.message });
+                    });
+                } else {
+                    res.status(500);
+                    res.json({ "status": "error", "msg": "Post cannot be found" });
+                }
+            } else {
+                res.status(500);
+                res.json({ "status": "error", "msg": err.message });
+            }
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });
+    } else {
+        res.status(500);
+        res.json({ "status": "error" });
+    }
+})
+
+
+
+
+// Event_images CRED
+app.get('/api/event_images', (req, res) => {
+    if (req.query.name && req.query.name != "" && req.query.image && req.query.image != "") {
+        EventImages.find({ name: req.query.image }).then(results => {
+            res.status(200);
+            res.json(results);
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });;
+    } else {
+        EventImages.find().then(results => {
+            res.status(200);
+            res.json(results);
+        });
+    }
+})
+
+app.post('/api/event_images', (req, res) => {
+    const newEventImage = EventImages();
+    newEventImage.name = req.body.name;
+    newEventImage.image = req.body.image;
+    newEventImage.save().then(() => {
+        res.status(200);
+        res.json({ "status": "ok" });
+    }).catch(err => {
+        res.status(500);
+        res.json({ "status": "error", "msg": err.message });
+    });;
+})
+
+// Event titles CRED
+app.get('/api/event_titles', (req, res) => {
+    // .query = asking for query parameter '?'
+    if (req.query.eventTitle && req.query.eventTitle != "") {
+        EventTitles.find({ eventTitle: req.query.eventTitle }).then(results => {
+            res.status(200);
+            res.json(results);
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });;
+    } else {
+        EventTitles.find().then(results => {
+            res.status(200);
+            res.json(results);
+        });
+    }
+})
+
+app.post('/api/event_titles', (req, res) => {
+    const newEventTitle = EventTitles();
+    newEventTitle.eventTitle = req.body.eventTitle;
+    newEventTitle.save().then(() => {
+        res.status(200);
+        res.json({ "status": "ok" });
+    }).catch(err => {
+        res.status(500);
+        res.json({ "status": "error", "msg": err.message });
+    });
+})
+
+app.put('/api/event_titles', (req, res) => {
+    const targetId = req.body._id;
+    var newValues = { $set: { eventTitle: req.body.eventTitle } }
+    EventTitles.updateOne({ "_id": targetId }, newValues).then(() => {
+        res.status(200);
+        res.json({ "status": "ok" });
+    }).catch(err => {
+        res.status(500);
+        res.json({ "status": "error", "msg": err.message });
+    });
+})
+
+app.delete('/api/event_titles', (req, res) => {
+    const targetId = req.query.id;
+    if (targetId && targetId != "") {
+        EventTitles.deleteOne({ "_id": targetId }).then(() => {
+            res.status(200);
+            res.json({ "status": "ok" });
+        }).catch(err => {
+            res.status(500);
+            res.json({ "status": "error", "msg": err.message });
+        });
+    } else {
+        res.status(500);
+        res.json({ "status": "error" });
+    }
+})
 
 // Otherwise, go to the FE
 app.listen(port, (err) => {
