2fc0a9c0a7d5e5c17cb762886bfc503b96d7e9d8
Heather Lee <leeh1102@gmail.com>
2022-10-02 08:05:02 -0700

    edit changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import { Component, state } from 'react';
+
 import './App.css';
 import SignIn from './components/SigninComponent';
 import MainComponent from './components/MainComponent';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import axios from "axios";
+
 
 
 
 function App() {
-  state = {
-    // title: '',
-    // location: '',
-    // description: ''
-    userName: "",
-    password: ""
-  }
-  // componentDidMount = () => {
-  //   this.getDataPosts();
-  // }
-  // getDataPosts = () => {
-  //   axios
-  //     .get("/api/users")
-  //     .then((response) => {
-  //       console.log(response);
-  //       const data = response.data;
-  //       this.setState({ posts: data });
-
-  //       console.log("Data has been received");
-  //     })
-  //     .catch(function (error) {
-  //       // handle error
-  //       console.log(error);
-  //     });
-  // }
   return (
     <Router>
       <Routes>
