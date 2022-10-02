baac8a512342477be73f09e4d524d5907ab78f02
msubin <joy120594@gmail.com>
2022-10-02 01:58:01 -0700

    Remove unused hero

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,37 +2,12 @@ import React from 'react';
 import Header from './HeaderComponent';
 import FilterComponent from './FilterComponent';
 import image from '../assets/background.png';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import { alpha } from '@material-ui/material';
 
 
 const MainComponent = () => {
     return (
         <>
             <Header />
-            <Box
-                classname="hero"
-                sx={{
-                    backgroundImage: `url(${image})`,
-                    backgroundSize: 'cover',
-                    pt: 8,
-                    pb: 6,
-                }}>
-                <Container maxwidth="sm">
-                    <Typography
-                        component="h2"
-                        variant="h2"
-                        align="center"
-                        color="white"
-                        backgroundColor={alpha('#000', 0.6)}
-                        gutterBottom
-                    >
-                        Welcome to SSUUUUP SAP!
-                    </Typography>
-                </Container>
-            </Box>
             <div className='hero'>
                 <img src={image} alt="hero-halloween" width="100%" />
             </div>
