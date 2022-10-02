d2578027ff7d28cfb003e4b1179fca6e86055fa4
msubin <joy120594@gmail.com>
2022-10-02 14:09:13 -0700

    Complete frame of Modal

--- src/components/CreateEventModalComponent.js
+++ src/components/CreateEventModalComponent.js
@@ -1,124 +1,188 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-// import Step from '@mui/material/Step';
 import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Backdrop from '@mui/material/Backdrop';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
 import styles from './create_event_modal_component.css';
-
-
-import { Stepper, Step } from "react-form-stepper";
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { useState } from "react";
+import { AddCircleOutline } from '@mui/icons-material';
+import { TextField } from '@mui/material';
+import { DesktopDatePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import DialogTitle from '@mui/material/DialogTitle';
+import MenuItem from '@mui/material/MenuItem';
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
+const amenities = [
+    {
+        value: 'PingPong',
+        label: 'PingPong',
+    },
+    {
+        value: 'BBQ',
+        label: 'BBQ',
+    },
+    {
+        value: 'Gym',
+        label: 'Gym',
+    },
+    {
+        value: 'Puzzle',
+        label: 'Puzzle',
+    },
+    {
+        value: 'D-Shop',
+        label: 'D-Shop',
+    },
+];
 
-
+const deliveryTypes = [
+    {
+        value: 'In-office',
+        label: 'In-office',
+    },
+    {
+        value: 'Virtual',
+        label: 'Virtual',
+    },
+    {
+        value: 'Out-of-office',
+        label: 'Out-of-office',
+    },
+];
 
 const CreateEventModalComponent = () => {
-    const [goSteps, setGoSteps] = useState(0);
-
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [time, setTime] = useState("");
-    const [date, setDate] = useState("");
-    const [amenity, setAmenity] = useState("");
+    const [values, setValues] = React.useState({
+        title: 'Join 3D Printing Workshop hosted by D-shop!',
+        description: 'Learn and experience 3D printing with our 3D printing experts!',
+        eventDate: '12/04/2022',
+        startTime: '15:00',
+        deliveryType: 'In-office',
+    })
 
-    const getTitle = () => {
-        const title = document.getElementById("amenities");
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('/api/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        handleClose();
     }
 
     return (
         <>
-            <div>
-                <Button onClick={handleOpen}>Open modal</Button>
-                <Modal
-                    aria-labelledby="transition-modal-title"
-                    aria-describedby="transition-modal-description"
-                    open={open}
-                    onClose={handleClose}
-                    closeAfterTransition
-                    BackdropComponent={Backdrop}
-                    BackdropProps={{
-                        timeout: 500,
-                    }}
+            <div className={styles.create_btn}>
+                <Button
+                    variant="contained"
+                    onClick={handleOpen}
+                    size="large"
+                    color="primary"
+                    sx={{ backgroundColor: "#006fbb", m: 2, mt: 4, p: 2, pr: 3, pl: 3, borderRadius: 20 }}
                 >
-                    <Fade in={open}>
-                        <Box sx={style}>
-                            <Typography id="transition-modal-title" variant="h6" component="h2">
-                                <div>
-                                    <Stepper activeStep={goSteps}>
-                                        <Step onClick={() => setGoSteps(0)} label="LOCATION" />
-                                        <Step onClick={() => setGoSteps(1)} label="DELIVERY" />
-                                        <Step onClick={() => setGoSteps(2)} label="ORDER SUMMERY" />
-                                    </Stepper>
-                                    {goSteps === 0 && (
-                                        <div>
-                                            <div>
-                                                <h3>Location</h3>
-                                                <p>What is the location? Select one.</p>
-                                                <select className={styles.input_tag} placeholder="Locations">
-                                                    <option value="montreal">Montreal</option>
+                    <AddCircleOutline sx={{ mr: 1 }} />
+                    Create Your Own
+                </Button>
+                <Dialog open={open} onClose={handleClose} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <DialogTitle>Optional sizes</DialogTitle>
+                    <DialogContent >
+                        <TextField
+                            name="title"
+                            required
+                            fullWidth
+                            id="title-input"
+                            label="Event Title"
+                            placeholder="Event Title"
+                            sx={{ mt: 1 }}
+                        />
+                        <TextField
+                            name="description"
+                            required
+                            fullWidth
+                            id="description-input"
+                            label="Description"
+                            placeholder="Description"
+                            sx={{ mt: 1 }}
+                        />
+                        <LocalizationProvider dateAdapter={AdapterDayjs} >
+                            <div sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <DesktopDatePicker
+                                    fullWidth
+                                    inputFormat="MM/DD/YYYY"
+                                    value={values.date}
+                                    onChange={(newValue) => {
+                                        setValues({ ...values, date: newValue });
+                                    }}
+                                    renderInput={(params) => <TextField {...params}
+                                        sx={{ mt: 1 }}
+                                    />}
+                                />
+                                <TimePicker
+                                    fullWidth
+                                    label="Time"
+                                    inputFormat="hh:mm"
+                                    onChange={(newValue) => {
+                                        setValues({ ...values, time: newValue });
+                                    }}
+                                    renderInput={(params) => <TextField {...params}
+                                        sx={{ mt: 1 }} />}
+                                />
+                            </div>
 
-
-                                                    {/* <option value="vacouver">Vancouver</option>
-                                                    <option value="montreal">Montreal</option>
-                                                    <option value="Calgary">Calgary</option>
-                                                    <option value="ottawa">Ottawa</option>
-                                                    <option value="waterloo">Waterloo</option>
-                                                    <option value="toronto">Toronto</option> */}
-
-                                                </select>
-                                                <h4>If in-office, select the amenity to use:</h4>
-                                                <select className={styles.input_tag} onChange={getTitle} placeholder="Amenities" id="amenities">
-                                                    <option value="PingPong">Ping Pong</option>
-                                                    <option value="BBQ">BBQ</option>
-                                                    <option value="Gym">Gym</option>
-                                                    <option value="Puzzle">Puzzle</option>
-                                                    <option value="DShop">D-Shop</option>
-
-                                                </select>
-                                            </div>
-                                            <button className="btn" onClick={() => setGoSteps(1)}>
-                                                Next
-                                            </button>
-                                        </div>
-                                    )}
-                                    {goSteps === 1 && (
-                                        <div>
-                                            Addreess
-                                            <button onClick={() => setGoSteps(2)}>Next</button>
-                                        </div>
-                                    )}
-                                    {goSteps === 2 && (
-                                        <div>
-                                            Payment
-                                            <button onClick={() => setGoSteps(3)}>Submit</button>
-                                        </div>
-                                    )}
-                                </div>                            </Typography>
-                            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                            </Typography>
-                        </Box>
-                    </Fade>
-                </Modal>
+                        </LocalizationProvider>
+                        <TextField
+                            name="deliveryType"
+                            fullWidth
+                            sx={{ mt: 1 }}
+                            id="select-delivery-types"
+                            label="Delivery Type">
+                        </TextField>
+                        <TextField
+                            select
+                            fullWidth
+                            onChange={(newValue) => {
+                                setValues({ ...values, amenities: newValue });
+                            }}
+                            id="select-amenities"
+                            sx={{ mt: 1 }}
+                            label="Amenities">
+                            {amenities.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            onClick={handleSubmit}
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 1, mb: 2, padding: 1.5, borderRadius: 10 }}
+                        >
+                            Create
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         </>
     );
